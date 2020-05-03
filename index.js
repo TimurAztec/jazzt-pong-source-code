28cf94a24f3618af7cff91b6a1503e341a3b446d
PING LORD <timotiaztec@gmail.com>
2020-05-03 20:59:48 +0300

    Tried to make roomed multiplayer

--- index.js
+++ index.js
@@ -4,212 +4,226 @@ const server = http.createServer((request, response) => {
     response.write('<h1>Hello world</h1>');
     response.end();
 }).listen(PORT);
+console.log('connect localhost:80');
 const EventEmitterCreator = require('events')
 const EventEmitter = new EventEmitterCreator();
 let io = require('socket.io').listen(server);
 io.set('origins', '*:*');
 
-let score = {
-    score1: 0,
-    score2: 0,
-    timeOut: false
-}
-let pause;
-let left;
-let chatStory = [];
-let theBall;
-let player1; let player2;
-let canvas = {
-    width: 650,
-    height: 400,
-}
-let gameLoop;
+const Rooms = io.sockets.adapter.rooms;
+const intervals = new Array();
 
 io.sockets.on('connection', (socket) => {
 
-    let playerNumber;
+    let roomId = 1;
+    function tryToJoin() {
+        socket.join(roomId.toString());
+        if (Rooms[roomId.toString()].length > 2) {
+            socket.leaveAll();
+            roomId += 1;
+            tryToJoin();
+        }
+    }
+    tryToJoin();
 
-    console.log(`${socket.handshake.address.address}:${socket.handshake.address.port} connected!`);
-    if (io.engine.clientsCount > 2) {
+    if (Rooms[roomId.toString()].length > 2) {
         socket.emit('full_server');
         socket.disconnect();
     } else {
+        console.log(`${socket.request.connection.remoteAddress} connected as ${socket.id} to room number ${roomId}`);
         socket.emit('connected');
     }
 
-    if (io.engine.clientsCount == 1) {
-        playerNumber = 1;
+    if (Rooms[roomId.toString()].length == 1) {
+        createRoom();
         if (Math.round(Math.random() * 1) == 0) {
-            left = true
+            Rooms[roomId.toString()].vars.left = true
             socket.emit('start_position', 'left');
         } else {
-            left = false;
+            Rooms[roomId.toString()].vars.left = false;
             socket.emit('start_position', 'right');
         }
     } else {
-        playerNumber = 2;
-        if (left) {
+        if (Rooms[roomId.toString()].vars.left) {
             socket.emit('start_position', 'right');
         } else {
             socket.emit('start_position', 'left');
         }
     }
 
-    if (io.engine.clientsCount == 2) {
-        io.sockets.emit('start');
+    if (Rooms[roomId.toString()].length == 2) {
+        io.in(roomId.toString()).emit('start');
         EventEmitter.emit('start');
     }
 
     socket.on('disconnect', () => {
-        socket.broadcast.emit('player_left_game');
-        gameEnd();
+        socket.broadcast.in(roomId.toString()).emit('player_left_game');
+        EventEmitter.emit('clear_rooms_intervals');
     });
 
     socket.on('cords', (data) => {
-        if (data.player1) { player1 = data.player1; } else if (data.player2) { player2 = data.player2; }
+        if (data.player1) { Rooms[roomId.toString()].vars.player1 = data.player1; } else if (data.player2) { Rooms[roomId.toString()].vars.player2 = data.player2; }
         socket.emit('pong');
-        socket.emit('score', { score1: score.score1, score2: score.score2 });
+        socket.emit('score', { score1: Rooms[roomId.toString()].vars.score.score1, score2: Rooms[roomId.toString()].vars.score.score2 });
     })
 
     socket.on('pause', () => {
-        socket.broadcast.emit('pause');
-        pause = true;
+        socket.broadcast.in(roomId.toString()).emit('pause');
+        Rooms[roomId.toString()].vars.pause = true;
     })
 
     socket.on('continue', () => {
-        socket.broadcast.emit('continue');
-        pause = false;
-    })
-
-    socket.on('add_score', (data) => {
-        if (!score.timeOut) {
-            if (data == 1) {
-                score.score1 += 1;
-            } else if (data == 2) {
-                score.score2 += 1;
-            }
-            score.timeOut = true; setTimeout(() => { score.timeOut = false; }, 350);
-        }
-        if (score.score1 >= 10) {
-            io.sockets.emit('end_game', 'Player 1 won the game!'); clearScore();
-        } else if (score.score2 >= 10) {
-            io.sockets.emit('end_game', 'Player 2 won the game!'); clearScore();
-        }
+        socket.broadcast.in(roomId.toString()).emit('continue');
+        Rooms[roomId.toString()].vars.pause = false;
     })
 
     socket.on('chat_message', (data) => {
         if (data) {
-            chatStory.push(data);
-            io.sockets.emit('chat_update', data);
+            Rooms[roomId.toString()].vars.chatStory.push(data);
+            io.in(roomId.toString()).emit('chat_update', data);
         }
     })
 
+    EventEmitter.on('start', () => {
+        createTheBall();
+        createPlayers();
+        setTimeout(() => {
+            Rooms[roomId.toString()].vars.gameLoop = setInterval(() => {
+                if (Rooms[roomId.toString()]) {
+                if (!Rooms[roomId.toString()].vars.pause) {
+                    ballBounce();
+                    scoreCheck();
+                    io.in(roomId.toString()).emit('server_cords', {
+                        theBall: Rooms[roomId.toString()].vars.theBall,
+                        player1: Rooms[roomId.toString()].vars.player1,
+                        player2: Rooms[roomId.toString()].vars.player2,
+                    });
+                } else {
+                    if (Rooms[roomId.toString()].vars.pause) {
+
+                    }
+                }
+                }
+                // console.log(new Date());
+            }, 1000/60);
+        }, 3000);
+    });
+
+    function createTheBall() {
+        Rooms[roomId.toString()].vars.theBall = {
+            x: (Rooms[roomId.toString()].vars.canvas.width / 2),
+            y: (Rooms[roomId.toString()].vars.canvas.height / 2),
+            width: 15,
+            height: 15,
+            color: '#FFFFFF',
+            speed: 1,
+            speedUp: 1,
+            gravity: 1
+        }
+    }
+
+    function createPlayers() {
+        Rooms[roomId.toString()].vars.player1 = {
+            x: 10,
+            y: 200,
+            width: 15,
+            height: 80,
+            color: '#FFFFFF',
+            gravity: 2
+        }
+        Rooms[roomId.toString()].vars.player2 = {
+            x: 625,
+            y: 100,
+            width: 15,
+            height: 80,
+            color: '#FFFFFF',
+            gravity: 2
+        }
+    }
+
+    function ballBounce() {
+        if (((Rooms[roomId.toString()].vars.theBall.y + Rooms[roomId.toString()].vars.theBall.gravity) <= 0) || ((Rooms[roomId.toString()].vars.theBall.y + Rooms[roomId.toString()].vars.theBall.gravity + Rooms[roomId.toString()].vars.theBall.height) >= Rooms[roomId.toString()].vars.canvas.height)) {
+            Rooms[roomId.toString()].vars.theBall.gravity = Rooms[roomId.toString()].vars.theBall.gravity * -1;
+            Rooms[roomId.toString()].vars.theBall.y += Rooms[roomId.toString()].vars.theBall.gravity;
+            Rooms[roomId.toString()].vars.theBall.x += Rooms[roomId.toString()].vars.theBall.speed;
+        } else {
+            Rooms[roomId.toString()].vars.theBall.x += Rooms[roomId.toString()].vars.theBall.speed;
+            Rooms[roomId.toString()].vars.theBall.y += Rooms[roomId.toString()].vars.theBall.gravity
+        }
+        ballCollision();
+    }
+
+    function ballCollision() {
+        if (((Rooms[roomId.toString()].vars.theBall.x + Rooms[roomId.toString()].vars.theBall.speed <= Rooms[roomId.toString()].vars.player1.x + Rooms[roomId.toString()].vars.player1.width) && (Rooms[roomId.toString()].vars.theBall.y + Rooms[roomId.toString()].vars.theBall.gravity > Rooms[roomId.toString()].vars.player1.y) && (Rooms[roomId.toString()].vars.theBall.y + Rooms[roomId.toString()].vars.theBall.gravity <= Rooms[roomId.toString()].vars.player1.y + Rooms[roomId.toString()].vars.player1.height))
+            || ((Rooms[roomId.toString()].vars.theBall.x + Rooms[roomId.toString()].vars.theBall.width + Rooms[roomId.toString()].vars.theBall.speed >= Rooms[roomId.toString()].vars.player2.x) && (Rooms[roomId.toString()].vars.theBall.y + Rooms[roomId.toString()].vars.theBall.gravity > Rooms[roomId.toString()].vars.player2.y) && (Rooms[roomId.toString()].vars.theBall.y + Rooms[roomId.toString()].vars.theBall.gravity <= Rooms[roomId.toString()].vars.player2.y + Rooms[roomId.toString()].vars.player2.height))) {
+            Rooms[roomId.toString()].vars.theBall.speed = Rooms[roomId.toString()].vars.theBall.speed * -1;
+            Rooms[roomId.toString()].vars.theBall.speed = Rooms[roomId.toString()].vars.theBall.speed * Rooms[roomId.toString()].vars.theBall.speedUp;
+            if (Rooms[roomId.toString()].vars.theBall.speedUp < 3) { Rooms[roomId.toString()].vars.theBall.speedUp = Rooms[roomId.toString()].vars.theBall.speedUp + 0.10; }
+        } else if (Rooms[roomId.toString()].vars.theBall.x + Rooms[roomId.toString()].vars.theBall.speed < Rooms[roomId.toString()].vars.player1.x) {
+            Rooms[roomId.toString()].vars.score.score2 ++;
+            // theBall.speed = theBall.speed * -1;
+            Rooms[roomId.toString()].vars.theBall.speed = 1; Rooms[roomId.toString()].vars.theBall.speedUp = 1;
+            Rooms[roomId.toString()].vars.theBall.x = 100 + Rooms[roomId.toString()].vars.theBall.speed;
+            Rooms[roomId.toString()].vars.theBall.y += Rooms[roomId.toString()].vars.theBall.gravity;
+        } else if (Rooms[roomId.toString()].vars.theBall.x + Rooms[roomId.toString()].vars.theBall.speed > Rooms[roomId.toString()].vars.player2.x + Rooms[roomId.toString()].vars.player2.width) {
+            Rooms[roomId.toString()].vars.score.score1 ++;
+            // theBall.speed = theBall.speed * -1;
+            Rooms[roomId.toString()].vars.theBall.speed = -1; Rooms[roomId.toString()].vars.theBall.speedUp = 1;
+            Rooms[roomId.toString()].vars.theBall.x = 500 + Rooms[roomId.toString()].vars.theBall.speed;
+            Rooms[roomId.toString()].vars.theBall.y += Rooms[roomId.toString()].vars.theBall.gravity;
+        } else {
+            Rooms[roomId.toString()].vars.theBall.x += Rooms[roomId.toString()].vars.theBall.speed;
+            Rooms[roomId.toString()].vars.theBall.y += Rooms[roomId.toString()].vars.theBall.gravity;
+        }
+    }
+
+    function scoreCheck() {
+        if (Rooms[roomId.toString()].vars.score.score1 >= 10) {
+            EventEmitter.emit('clear_rooms_intervals');
+            io.in(roomId.toString()).emit('end_game', 'Player 1 won the game!');
+        } else if (Rooms[roomId.toString()].vars.score.score2 >= 10) {
+            EventEmitter.emit('clear_rooms_intervals');
+            io.in(roomId.toString()).emit('end_game', 'Player 2 won the game!');
+        }
+    }
+
+    function createRoom() {
+        Rooms[roomId.toString()].vars = {
+            score: {
+                score1: 0,
+                score2: 0,
+                timeOut: false
+            },
+            pause: false,
+            left: false,
+            chatStory: [],
+            theBall: {},
+            player1: {}, player2: {},
+            canvas: {
+                width: 650,
+                height: 400,
+            },
+        }
+    }
+
 })
 
-EventEmitter.on('start', () => {
-    createTheBall();
-    createPlayers();
-    setTimeout(() => {
-        gameLoop = setInterval(() => {
-            if (!pause) {
-                ballBounce();
-                scoreCheck();
-                io.sockets.emit('server_cords', {
-                    theBall: theBall,
-                    player1: player1,
-                    player2: player2,
-                });
-            } else {
-                if (pause) {
+EventEmitter.on('clear_rooms_intervals', () => {
+    findCleanRoom();
+})
 
-                }
-            }
-            this.close
-        }, 1000/60);
-    }, 3000);
-});
-
-function clearScore() {
-    score = { score1: 0, score2: 0, timeOut: false }
-    left = false;
-}
-
-function createTheBall() {
-    theBall = {
-        x: (canvas.width / 2),
-        y: (canvas.height / 2),
-        width: 15,
-        height: 15,
-        color: '#FFFFFF',
-        speed: 1,
-        speedUp: 1,
-        gravity: 1
+function findCleanRoom() {
+    const RoomsKeysUnfiltered = Object.keys(Rooms);
+    const RoomsKeys = new Array();
+    // RoomsKeys.filter((key) => { return new RegExp('[0-255]').test(key); });
+    RoomsKeysUnfiltered.forEach(key => {
+        if (new RegExp('[0-255]').test(key)) { RoomsKeys.push(key); }
+    });
+    for (let i=0 ; i < RoomsKeys.length ; i++) {
+        if (Rooms[RoomsKeys[i]].length <= 1) {
+            // console.log(Rooms[RoomsKeys[i]].vars);
+            if (Rooms[RoomsKeys[i]] && Rooms[RoomsKeys[i]].vars) { clearInterval(Rooms[RoomsKeys[i]].vars.gameLoop); }
+            // console.log(Rooms[RoomsKeys[i]]);
+        }
     }
-}
-
-function createPlayers() {
-    player1 = {
-        x: 10,
-        y: 200,
-        width: 15,
-        height: 80,
-        color: '#FFFFFF',
-        gravity: 2
-    }
-    player2 = {
-        x: 625,
-        y: 100,
-        width: 15,
-        height: 80,
-        color: '#FFFFFF',
-        gravity: 2
-    }
-}
-
-function ballBounce() {
-    if (((theBall.y + theBall.gravity) <= 0) || ((theBall.y + theBall.gravity + theBall.height) >= canvas.height)) {
-        theBall.gravity = theBall.gravity * -1;
-        theBall.y += theBall.gravity;
-        theBall.x += theBall.speed;
-    } else {
-        theBall.x += theBall.speed;
-        theBall.y += theBall.gravity
-    }
-    ballCollision();
-}
-
-function ballCollision() {
-    if (((theBall.x + theBall.speed <= player1.x + player1.width) && (theBall.y + theBall.gravity > player1.y) && (theBall.y + theBall.gravity <= player1.y + player1.height))
-        || ((theBall.x + theBall.width + theBall.speed >= player2.x) && (theBall.y + theBall.gravity > player2.y) && (theBall.y + theBall.gravity <= player2.y + player2.height))) {
-        theBall.speed = theBall.speed * -1;
-        theBall.speed = theBall.speed * theBall.speedUp;
-        if (theBall.speedUp < 3) { theBall.speedUp = theBall.speedUp + 0.10; }
-    } else if (theBall.x + theBall.speed < player1.x) {
-        score.score2 ++;
-        // theBall.speed = theBall.speed * -1;
-        theBall.speed = 1; theBall.speedUp = 1;
-        theBall.x = 100 + theBall.speed;
-        theBall.y += theBall.gravity;
-    } else if (theBall.x + theBall.speed > player2.x + player2.width) {
-        score.score1 ++;
-        // theBall.speed = theBall.speed * -1;
-        theBall.speed = -1; theBall.speedUp = 1;
-        theBall.x = 500 + theBall.speed;
-        theBall.y += theBall.gravity;
-    } else {
-        theBall.x += theBall.speed;
-        theBall.y += theBall.gravity;
-    }
-}
-
-function scoreCheck() {
-    if (score.score1 >= 10) {
-        io.sockets.emit('end_game', 'Player 1 won the game!'); gameEnd();
-    } else if (score.score2 >= 10) {
-        io.sockets.emit('end_game', 'Player 2 won the game!'); gameEnd();
-    }
-}
-
-function gameEnd() {
-    clearScore(); clearInterval(gameLoop);
 }
